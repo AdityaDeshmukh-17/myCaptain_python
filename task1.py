5cb14d449b9a0a1ad11f538da4bee25448445305
Aditya Deshmukh <74229857+AdityaDeshmukh-17@users.noreply.github.com>
2021-04-09 13:22:15 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+#python 3.7.1
+
+print ("***********************")
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
